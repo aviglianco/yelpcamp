ab14c4707d2e96801891d965d4eb761f9e6d499a
aviglianco <aviglianco22@gmail.com>
2021-03-07 14:17:48 -0300

    New camp working

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get("/campgrounds", function(req, res){
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
@@ -39,7 +39,8 @@ app.get("/campgrounds", function(req, res){
 app.post("/campgrounds", function(req, res) {
     var newCampName = req.body.newCampName;
     var newCampImg = req.body.newCampImg;
-    var newCampground = {name: newCampName, img: newCampImg}
+    var newCampDesc = req.body.newCampDesc;
+    var newCampground = {name: newCampName, img: newCampImg, description: newCampDesc}
     Campground.create(newCampground, function(err, newCamp) {
         if (err) {
             console.log(err);
@@ -51,7 +52,17 @@ app.post("/campgrounds", function(req, res) {
 
 app.get("/campgrounds/new", function(req, res) {
     res.render("new-camp");
-})
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
 app.listen(3000, function() {
     console.log("Server running on port 3000.");
